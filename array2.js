e1662a63003d3ea32dfa8f6111b3b85dbf43c710
ctartt12 <consuellat12@gmail.com>
2025-09-10 05:26:51 +0000

    complete

---
+++ array2.js
@@ -0,0 +1,18 @@
+//task1
+const array1 = Array(7)
+console.log(array1)
+const array2 = Array(7).fill("Hello")
+console.log('final Array: ', array2) 
+
+//task2 
+const array3 = Array(3)
+console.log(array3)
+const array4 = Array(3).fill("froggy")
+console.log('final array: ', array4)
+
+//task3 
+const students = Array(5) 
+for (let i = 0; i < students.length; i++) {
+    console.log(`${students[i]}'s id is ${i * 10}`)
+}
+
